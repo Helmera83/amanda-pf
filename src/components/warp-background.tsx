e3dea73c841ae4467d0348c1b5995ab53f7187e8
amanda <helmeramanda90@gmail.com>
2025-11-30 11:30:47 -0600

    1

--- src/components/warp-background.tsx
+++ src/components/warp-background.tsx
@@ -1,10 +1,16 @@
 import { Warp, type WarpProps } from '@paper-design/shaders-react';
 
 export default function WarpBackground(props: WarpProps) {
-    
+
     const defaultProps = {
-        speed: 0.4,
-        rotation: 0.5,
+        speed: 0.3,
+        rotation: 0.3,
+        swirl: 0.8,
+        swirlIterations: 9,
+        shapeScale: 0.3,
+        color1: 'rgba(0,0,0,0.7)',
+        color2: '#2512a2',
+        color3: '#fd4e03',
         style: { width: '100%', height: '100%' }
     };
 
