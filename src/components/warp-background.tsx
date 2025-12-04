06c211add035801dd2b2a7b2d26e708b2c1e8660
amanda <helmeramanda90@gmail.com>
2025-12-04 10:05:20 -0600

    1

--- src/components/warp-background.tsx
+++ src/components/warp-background.tsx
@@ -4,11 +4,11 @@ export default function WarpBackground(props: WarpProps) {
 
     const defaultProps = {
         speed: 0.3,
-        rotation: 0.3,
-        swirl: 0.8,
-        swirlIterations: 9,
-        shapeScale: 0.3,
-        color1: 'rgba(0,0,0,0.7)',
+        rotation: 0.9,
+        swirl: 0.5,
+        swirlIterations: 6,
+        shapeScale: 0.1,
+        color1: 'rgba(250,250,250,0.2)',
         color2: '#2512a2',
         color3: '#fd4e03',
         style: { width: '100%', height: '100%' }
